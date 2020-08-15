49904cfc1c478d6dd4bee3a4c52f03f29767d72e
Oguzhan INAN <oguzhan3488@gmail.com>
2020-08-16 01:22:26 +0300

    Add resize support to sidebar #15

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,26 @@
 <script>
+const sidebarSize = { max: 600, min: 350 }
+
 import Connections from '@/components/connections.vue'
 
 export default {
   name: 'App',
+
   components: {
     Connections
+  },
+
+  methods: {
+    mouseDown(e) {
+      this.isSidebarResize = true
+      this.startX = this.$refs.scrollContainer.offsetWidth - e.pageX
+    },
+    mouseMove(e) {
+      const width = this.startX + e.pageX
+      if (this.isSidebarResize && width < sidebarSize.max && width > sidebarSize.min) {
+        this.$refs.scrollContainer.style.width = `${width}px`
+      }
+    }
   }
 }
 </script>
@@ -13,8 +29,9 @@ export default {
   #app
     .title-bar
       | Mogollar
-    .views
-      .scroll-container
+    .views(@mousemove="mouseMove" @mouseup="isSidebarResize = false")
+      .scroll-container(ref="scrollContainer")
+        .resize-handle(@mousedown="mouseDown")
         .top
         .scrollable
           aside
@@ -40,8 +57,8 @@ body {
 }
 
 #app {
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
-    'Helvetica Neue', sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
+    'Open Sans', 'Helvetica Neue', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   height: 100%;
@@ -58,7 +75,7 @@ body {
   font-size: var(--sm-font-size);
   // font-weight: var(--book);
   text-align: center;
-  z-index: 1;
+  z-index: 9;
 }
 
 .views {
@@ -71,10 +88,27 @@ body {
 }
 
 .scroll-container {
+  width: 400px;
+  position: relative;
   padding: 98px 0 0;
   overflow: scroll;
   scroll-snap-type: y mandatory;
   scroll-padding: 38px 0 0;
+
+  .resize-handle {
+    content: '';
+    width: 6px;
+    height: 80px;
+    cursor: col-resize;
+    position: absolute;
+    right: 0px;
+    top: calc(50% + 38px);
+    z-index: 9;
+    border-radius: 3px;
+    background-color: var(--box-bg);
+    user-select: none;
+    -webkit-user-drag: none;
+  }
 }
 
 .scrollable {
@@ -82,6 +116,12 @@ body {
   scroll-snap-type: center;
   display: table;
   min-height: calc(100% + 98px);
+  min-width: 350px;
+
+  &,
+  aside {
+    width: 100%;
+  }
 }
 
 .scrollable * {
