2e02373863cc7d8e2ec67e01501b3f17c0e156ba
Recep Çil <recepcil95@hotmail.com>
2020-08-31 00:49:43 +0300

    Revert "Revert "Merge branch 'master' into toggle-connection-section""
    This reverts commit b59d964275d27585389aa674a8c63298f08795a4.

--- src/components/connections.vue
+++ src/components/connections.vue
@@ -14,14 +14,14 @@ export default {
     }
   },
   methods: {
-    ...mapActions(['connect', 'setCollection']),
+    ...mapActions(['connect', 'setDatabase', 'setCollection']),
     async doConnect() {
       await this.connect(this.connectionString)
       this.$router.push('/connection')
     }
   },
   computed: {
-    ...mapState(['collections'])
+    ...mapState(['databases', 'collections'])
   }
 }
 </script>
@@ -41,10 +41,23 @@ export default {
         input(type="text" v-model="connectionString")
       button(type="button" @click="doConnect") Connect
   .box
+     h1 Databases
+     .database(v-for="database in databases" @click="setDatabase(database.name)")
+       p {{database.name}}
+  .box
     h1 Collections
     .collection(v-for="collection in collections" @click="setCollection(collection.name)")
       p {{collection.name}}
 
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.collection {
+  cursor: pointer;
+  color: var(--fg-dim-color);
+  transition: var(--transition);
+}
+.collection:hover {
+  color: var(--fg-color);
+}
+</style>
